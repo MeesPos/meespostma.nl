223d15420abd55bca450469c868581d4e046376a
MeesPos <meespostma_@hotmail.com>
2022-09-19 14:29:29 +0200

    Added About me header section

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -1,7 +1,34 @@
 <template>
-    
-</template>
+  <main>
+    <div class="my-12 w-7/12">
+      <h2 class="text-4xl font-medium">
+        Lorem ipsum dolor sit amet dictum, ut al adipiscing elit.
+      </h2>
 
-<script setup>
-    
-</script>
+      <p class="leading-6 mt-6">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et nisl
+        sit amet metus consequat mollis. In non est sed ipsum auctor aliquam.
+        Maecenas nec feugiat arcu, sed ornare felis. Pellentesque neque felis,
+        iaculis nec tempus at, mollis ac lectus. Integer in aliquam justo. Sed
+        diam nulla, porttitor sit amet volutpat quis, ultricies ut massa.
+        Vivamus ex urna, ornare id placerat sit amet, laoreet sit amet diam. Ut
+        ut purus a velit ullamcorper venenatis.<br />
+        <br />
+
+        Maecenas nisi orci, venenatis sit amet dui vel, semper rutrum tortor.
+        eleifend vel erat sed, malesuada accumsan sapien. Vivamus id orci ac
+        magna elementum iaculis. Nulla arcu nisl, scelerisque et laoreet nec,
+        porttitor nec sapien. Quisque mollis, dui eget vehicula sodales, velit
+        metus fringilla eros, ut egestas massa purus quis nulla. In condimentum
+        in ex vitae rhoncus. Vivamus a lacus sem. Sed ultrices purus leo, nec
+        tempus mauris dapibus nec. Integer sagittis lacinia dui, tincidunt
+        sodales eros facilisis sed. Nulla quam ante, feugiat quis varius a,
+        faucibus ut dolor.
+      </p>
+
+      <NuxtLink to="#">
+        <Button title="Mijn CV" class="mt-6 px-10" />
+      </NuxtLink>
+    </div>
+  </main>
+</template>
