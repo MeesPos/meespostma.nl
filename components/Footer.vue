e164b202607fc9f3768bb3054a4bb40ec8d61315
MeesPos <meespostma_@hotmail.com>
2022-09-19 14:09:32 +0200

    Added footer styling

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,38 @@
 <template>
-    
+  <div class="mt-16">
+    <div class="w-full border-t border-zinc-100" />
+
+    <div class="max-w-7xl mx-auto my-6 flex w-full">
+      <div class="flex flex-wrap gap-6 font-bold grow">
+        <a
+          v-for="(navItem, i) in navItems"
+          :key="i"
+          :href="navItem.href"
+          class=""
+          v-text="navItem.title"
+        />
+      </div>
+
+      <div class="text-right grow text-zinc-400">
+        <p>&copy; {{ currentYear }} Mees Postma. All rights reserved.</p>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+    const navItems = ref([
+        {
+            title: 'Over mij',
+            href: '#',
+            active: false,
+        },
+        {
+            title: 'Projecten',
+            href: '#',
+            active: false,
+        },
+    ])
+
+    const currentYear = new Date().getFullYear()
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="w-screen h-full bg-zinc-50">
-        <div class="max-w-screen-2xl h-full mx-auto bg-white">
+    <div class="w-screen min-h-screen bg-zinc-50">
+        <div class="max-w-screen-2xl min-h-screen mx-auto bg-white">
             <div class="max-w-7xl mx-auto">
                 <Navigation />
 
