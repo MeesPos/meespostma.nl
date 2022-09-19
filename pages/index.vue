5465ecb4fe3bd7ca84429bc3e432e6e38c34b73f
MeesPos <meespostma_@hotmail.com>
2022-09-19 12:13:22 +0200

    Added assets and basic layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,22 @@
 <template>
     <div>
-        <h2 class="text-4xl font-medium">Software developer, enthousiaste reiziger en nog iets in te vullen.</h2>
+        <div>
+            <h2 class="text-4xl font-medium">Software developer, enthousiaste reiziger en nog iets in te vullen.</h2>
 
-        <p class="leading-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lobortis dolor a nisl venenatis lobortis.
-            Fusce lacinia tempus mauris, a faucibus elit tristique quis. Vestibulum sagittis purus vel augue rutrum,
-            eget euismod sem hendrerit. Etiam blandit lectus eu mi molestie sagittis. Sed lobortis porttitor dolor,
-            vitae tempor ex suscipit eu</p>
+            <p class="leading-6 mt-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lobortis dolor a nisl venenatis lobortis.
+                Fusce lacinia tempus mauris, a faucibus elit tristique quis. Vestibulum sagittis purus vel augue rutrum,
+                eget euismod sem hendrerit. Etiam blandit lectus eu mi molestie sagittis. Sed lobortis porttitor dolor,
+                vitae tempor ex suscipit eu</p>
 
-        <a href="#">
-            <Button title="Over mij" />
-        </a>
+            <a href="#">
+                <Button title="Over mij"
+                        class="mt-4"
+                />
+            </a>
+        </div>
+
+        <div>
+            <img src="assets/mees.jpeg" />
+        </div>
     </div>
 </template>
