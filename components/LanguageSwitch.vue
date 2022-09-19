fabed63de3c8f49f20812e8a693902f3cb8acf5d
MeesPos <meespostma_@hotmail.com>
2022-09-19 11:01:18 +0200

    Added basic styling and openModal javascript code

--- components/LanguageSwitch.vue
+++ components/LanguageSwitch.vue
@@ -1,11 +1,27 @@
 <template>
-    <div>
-        <span class="">NL</span>
+    <div class="text-zinc-800 font-medium">
+        <div class="flex gap-1 items-center cursor-pointer" @click="setOpenModal">
+            <span class="">NL</span>
 
-        <Icon name="heroicons-solid:chevron-down" />
+            <Icon name="heroicons-solid:chevron-down"
+                  :class="openModal ? 'rotate-180' : ''"
+            />
+        </div>
+
+        <div v-if="openModal">
+            <h1>Test</h1>
+        </div>
     </div>
 </template>
 
 <script setup>
+    let openModal = ref(false);
 
+    function setOpenModal() {                
+        if(openModal.value) {
+            return openModal.value = false;
+        }
+
+        return openModal.value = true;
+    }
 </script>
