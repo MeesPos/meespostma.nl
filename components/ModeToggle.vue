1061127a6c28264cc73fee0de5ac0ba587172102
MeesPos <meespostma_@hotmail.com>
2022-09-19 11:16:55 +0200

    Added Dark and Light switch

--- components/ModeToggle.vue
+++ components/ModeToggle.vue
@@ -1,7 +1,25 @@
 <template>
+    <div class="cursor-pointer" @click="setDarkMode">
+        <Icon v-if="darkMode"
+              name="heroicons-outline:moon"
+              class="w-6 h-6 text-yellow-300"
+        />
 
+        <Icon v-else
+              name="heroicons-outline:sun"
+              class="w-6 h-6 text-zinc-800"
+        />
+    </div>
 </template>
 
 <script setup>
-    
+    let darkMode = ref(false);
+
+    function setDarkMode() {
+        if (darkMode.value) {
+            return darkMode.value = false;
+        }
+
+        return darkMode.value = true;
+    }
 </script>
