aa58532dacacd66f3f16ed82dedb981c5d01ef24
MeesPos <meespostma_@hotmail.com>
2022-09-19 11:11:58 +0200

    Added Language Switch styling

--- components/LanguageSwitch.vue
+++ components/LanguageSwitch.vue
@@ -3,13 +3,21 @@
         <div class="flex gap-1 items-center cursor-pointer" @click="setOpenModal">
             <span class="">NL</span>
 
-            <Icon name="heroicons-solid:chevron-down"
-                  :class="openModal ? 'rotate-180' : ''"
-            />
+            <Icon name="heroicons-solid:chevron-down" :class="openModal ? 'rotate-180' : ''" />
         </div>
 
-        <div v-if="openModal">
-            <h1>Test</h1>
+        <div v-if="openModal" class="absolute z-10 mt-3 max-w-5 -translate-x-1/2 transform px-2 sm:px-0">
+            <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                <div class="relative grid gap-6 bg-white px-5 sm:px-5 sm:py-3 my-2">
+                    <a href="#" class="-m-3 block rounded-md p-3 transition duration-150 ease-in-out hover:bg-gray-50">
+                        <p class="text-base font-medium text-gray-900">Nederlands</p>
+                    </a>
+
+                    <a href="#" class="-m-3 block rounded-md p-3 transition duration-150 ease-in-out hover:bg-gray-50">
+                        <p class="text-base font-medium text-gray-900">English</p>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -17,8 +25,8 @@
 <script setup>
     let openModal = ref(false);
 
-    function setOpenModal() {                
-        if(openModal.value) {
+    function setOpenModal() {
+        if (openModal.value) {
             return openModal.value = false;
         }
 
