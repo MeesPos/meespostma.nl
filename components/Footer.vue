91ce11c0073c2cf17c2a906a7ecaa5088abfec9f
MeesPos <meespostma_@hotmail.com>
2022-09-20 11:53:02 +0200

    Made footer responsive

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,8 +2,8 @@
   <div class="mt-16">
     <div class="w-full border-t border-zinc-100" />
 
-    <div class="max-w-7xl mx-auto py-6 flex w-full">
-      <div class="flex flex-wrap gap-6 font-bold grow">
+    <div class="max-w-7xl mx-auto py-6 flex flex-col sm:flex-row w-full">
+      <div class="flex flex-wrap gap-6 font-bold grow mx-auto">
         <NuxtLink v-for="(navItem, i) in navItems"
             :key="i"
             :to="navItem.href"
@@ -12,7 +12,7 @@
         </NuxtLink>
       </div>
 
-      <div class="text-right grow text-zinc-400">
+      <div class="sm:text-right text-center grow text-zinc-400 mt-4 sm:mt-0">
         <p>&copy; {{ new Date().getFullYear() }} Mees Postma. All rights reserved.</p>
       </div>
     </div>
