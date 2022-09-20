a44039813a83d163dba28e4a33fa56dd2c3edef1
MeesPos <meespostma_@hotmail.com>
2022-09-20 11:49:15 +0200

    Made contact section responsive

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="max-w-2xl mx-auto">
-        <h2 class="text-center text-4xl font-medium">Neem contact op</h2>
+    <div class="sm:max-w-2xl w-11/12 mx-auto">
+        <h2 class="text-xl sm:text-4xl font-medium mt-4 sm:mt-0 text-center">Neem contact op</h2>
 
         <p class="mt-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lobortis dolor a nisl venenatis lobortis. Fusce lacinia tempus mauris, a faucibus elit tristique quis.</p>
 
-        <div class="flex gap-12 justify-center mt-6">
+        <div class="flex gap-6 sm:gap-12 justify-center mt-6">
             <a href="mailto:mail@meespostma.nl">
-                <Button title="E-mail" class="px-12" />
+                <Button title="E-mail" class="px-6 sm:px-12" />
             </a>
 
             <a href="https://nl.linkedin.com/in/mees-postma-a911b1196" target="_blank">
-                <Button title="LinkedIn" class="px-12" />
+                <Button title="LinkedIn" class="px-6 sm:px-12" />
             </a>
         </div>
     </div>
