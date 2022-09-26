27206f345f5678ac883b19c7c980143a2bcbbb55
MeesPos <meespostma_@hotmail.com>
2022-09-26 11:00:16 +0200

    Added basic inputs

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,17 @@
 <template>
+    <div>
+        <div>
+            <label for="email">E-mailadres</label>
+            <input id="email" />
+        </div>
 
+        <div>
+            <label for="password">Wachtwoord</label>
+            <input id="email" />
+        </div>
+    </div>
 </template>
 
 <script setup>
-    
+
 </script>
