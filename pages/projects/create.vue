352654b565796ea5499a0c2cf22e885a435591a4
MeesPos <meespostma_@hotmail.com>
2022-10-03 10:59:31 +0200

    Added button styling and catch for errors

--- pages/projects/create.vue
+++ pages/projects/create.vue
@@ -42,9 +42,11 @@
                     </div>
 
                     <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-                        <label for="urlPlaceholder" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">URL Placeholder</label>
+                        <label for="urlPlaceholder" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">URL
+                            Placeholder</label>
                         <div class="mt-1 sm:col-span-2 sm:mt-0">
-                            <input type="text" name="urlPlaceholder" id="urlPlaceholder" autocomplete="urlPlaceholder" v-model="urlPlaceholder"
+                            <input type="text" name="urlPlaceholder" id="urlPlaceholder" autocomplete="urlPlaceholder"
+                                v-model="urlPlaceholder"
                                 class="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm">
                         </div>
                     </div>
@@ -54,10 +56,16 @@
 
         <div class="pt-5">
             <div class="flex justify-end">
-                <button type="button"
-                    class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Cancel</button>
-                <button type="submit"
-                    class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
+                <NuxtLink to="/dashboard">
+                    <Button class="border-gray-300 bg-white text-sm text-gray-700 hover:bg-gray-50" 
+                            title="Cancel"
+                    />
+                </NuxtLink>
+
+                <Button type="submit" 
+                        class="ml-3 text-sm"
+                        title="Save" 
+                />
             </div>
         </div>
     </form>
@@ -84,7 +92,7 @@
 
         reader.readAsDataURL(file);;
 
-        reader.onload = function() {
+        reader.onload = function () {
             logo.value = reader.result;
         }
 
@@ -92,16 +100,23 @@
     }
 
     function submit() {
-        $apiFetch('/Projects', {
-            method: 'POST',
-            body: {
-                name: name.value,
-                description: description.value,
-                logo: logo.value.substring(logo.value.length, 23),
-                url: url.value,
-                urlPlaceholder: urlPlaceholder.value,
-                OriginalLogoName: OriginalLogoName 
-            }
-        })
+        try {
+            $apiFetch('/Projects', {
+                method: 'POST',
+                body: {
+                    name: name.value,
+                    description: description.value,
+                    logo: logo.value.substring(logo.value.length, 23),
+                    url: url.value,
+                    urlPlaceholder: urlPlaceholder.value,
+                    OriginalLogoName: OriginalLogoName
+                }
+            })
+
+            // window.location.pathname = '/dashboard';
+        } catch (err) {
+            console.log(err);
+        }
+
     }
 </script>
