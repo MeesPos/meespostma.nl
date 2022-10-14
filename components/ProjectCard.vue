bc6aaea6c9b948770971b38abda9d374ba1c8927
MeesPos <meespostma_@hotmail.com>
2022-10-14 11:53:02 +0200

    Added fetch for projects on index page

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,27 +1,30 @@
 <template>
+    <div>
     <a :href="project.url" target="_blank">
         <div class="bg-white rounded-full w-10 h-10 shadow-md relative">
             <img :src="project.logo" class="w-8 h-8 left-1 top-1 absolute">
         </div>
 
-        <h3 class="font-bold text-lg mt-4" v-text="project.title" />
+        <h3 class="font-bold text-lg mt-4" v-text="project.name" />
 
         <p class="mt-4 leading-7" v-text="project.description" />
 
         <div :href="project.url" class="mt-4 flex items-center gap-2 text-zinc-400">
             <Icon name="heroicons-solid:link" />
-            <a :href="project.url" v-text="project.url" />
+            <a target="_blank" :href="project.url" v-text="project.urlPlaceholder" />
         </div>
     </a>
+</div>
 </template>
 
 <script setup>
     defineProps({
         project: {
             logo: '',
-            title: '',
+            name: '',
             description: '',
-            url: ''
+            url: '',
+            urlPlaceholder: ''
         }
     })
 </script>
