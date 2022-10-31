7b01d4264f61876ac2b59cbd20b83aba63b17cfb
MeesPos <meespostma_@hotmail.com>
2022-10-31 10:12:22 +0100

    Added projects page HTML content

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,5 +1,21 @@
 <template>
-    
+    <main>
+        <div class="my-12 mx-auto xl:ml-0 xl:w-7/12 w-11/12">
+            <h2 class="text-xl sm:text-4xl font-medium mt-4 md:mt-0" v-text="$t('projects.header.title')" />
+
+            <p class="leading-6 mt-4 sm:mt-6" v-text="$t('projects.header.description')" />
+        </div>
+
+        <div class="mb-16 mt-8 sm:mt-0 w-11/12 xl:w-full mx-auto">
+            <div class="flex flex-col sm:flex-row gap-8 sm:gap-32 mt-8">
+                <ProjectCard
+                    v-for="project in projects"
+                    :key="project.id"
+                    :project="project"
+                />
+            </div>
+        </div>
+  </main>
 </template>
 
 <script setup>
