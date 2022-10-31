20f29c3324ebad77b7d00b7373bc6eb2c6e3430b
MeesPos <meespostma_@hotmail.com>
2022-10-31 10:54:00 +0100

    Removed Nuxt Auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,10 +7,7 @@ export default defineNuxtConfig({
         '@nuxtjs/tailwindcss',
         'nuxt-icon',
         '@intlify/nuxt3',
-        '@nuxtjs/color-mode',
-        '@nuxtjs-alt/auth',
-        '@nuxtjs-alt/http',
-        '@pinia/nuxt'
+        '@nuxtjs/color-mode'
     ],
     colorMode: {
         classSuffix: ''
@@ -19,18 +16,6 @@ export default defineNuxtConfig({
         localeDir: 'locales',
         vueI18n: {
             locale: 'nl'
-        }
-    },
-    auth: {
-        strategies: {
-            localStorage: false,
-            local: {
-                endpoints: {
-                    csrf: false,
-                    login: { url: `${process.env.API_URL}/Auth/login`, method: 'post' },
-                    user: false
-                }
-            },
         }
     }
 })
