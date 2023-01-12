7f3e1ee47ab74a2ccd87835bc5a59a6f4756a60b
Mees Postma <mail@meespostma.nl>
2023-01-12 20:39:50 +0100

    Added Navigation component

---
+++ components/navigation.tsx
@@ -0,0 +1,52 @@
+export default function Navigation() {
+  const navItems: Array<{
+    title: string;
+    href: string;
+  }> = [
+    {
+      title: "pages.about-me",
+      href: "/about-me",
+    },
+    {
+      title: "pages.projects",
+      href: "/projects",
+    },
+  ];
+
+  return (
+    <section className="w-full bg-white dark:bg-zinc-900">
+      <div className="container flex flex-col flex-wrap items-center justify-between py-8 mx-auto md:flex-row">
+        <div className="relative flex flex-col md:flex-row">
+          <a
+            href="/"
+            className="flex items-center mb-5 font-bold text-gray-900 dark:text-white lg:w-auto lg:items-center lg:justify-center md:mb-0"
+          >
+            <span className="mx-auto text-xl leading-none select-none">
+              Mees Postma
+            </span>
+          </a>
+
+          <nav className="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-zinc-100">
+            {navItems.map((navItem, index) => {
+              return (
+                <a
+                  key={index}
+                  href={navItem.href}
+                  className="mx-4 md:mx-0 md:mr-8 leading-6 text-black hover:text-gray-900 dark:text-white"
+                >
+                  $t({navItem.title})
+                </a>
+              );
+            })}
+          </nav>
+        </div>
+
+        <div className="inline-flex items-center md:ml-5 space-x-6 lg:justify-end">
+          {/* <LanguageSwitch /> */}
+
+          {/* <ModeToggle /> */}
+        </div>
+      </div>
+    </section>
+  );
+}
--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,3 +1,5 @@
+import Navigation from "../components/navigation";
+
 export default function DefaultLayout({
   children,
 }: {
@@ -7,7 +9,7 @@ export default function DefaultLayout({
     <div className="min-h-screen bg-zinc-50 dark:bg-black dark:text-white">
       <div className="max-w-screen-2xl min-h-screen mx-auto bg-white dark:bg-zinc-900">
         <div className="max-w-7xl mx-auto">
-          {/* <Navigation /> */}
+          <Navigation />
 
           {children}
         </div>
