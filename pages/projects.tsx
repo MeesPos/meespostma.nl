c403a6fd4b235a487416b3fc399670c51e65542a
Mees Postma <mail@meespostma.nl>
2023-01-13 17:55:26 +0100

    Added Next i18n

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import ProjectCard from "../components/projectCard";
 import DefaultLayout from "../layouts/default";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 export default function Project() {
   const [projects, setProjects] = useState<Array<any>>([]);
+
+  const { t } = useTranslation("common");
 
   useEffect(() => {
     async function getDatabase() {
@@ -20,11 +24,11 @@ export default function Project() {
       <main>
         <div className="my-12 mx-auto xl:ml-0 xl:w-7/12 w-11/12">
           <h2 className="text-xl sm:text-4xl font-medium mt-4 md:mt-0">
-            $t('projects.header.title')
+            {t("projects.header.title")}
           </h2>
 
           <p className="leading-6 mt-4 sm:mt-6">
-            $t('projects.header.description')
+            {t("projects.header.description")}
           </p>
         </div>
 
@@ -38,4 +42,13 @@ export default function Project() {
       </main>
     </DefaultLayout>
   );
+}
+
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Will be passed to the page component as props
+    },
+  };
 }
