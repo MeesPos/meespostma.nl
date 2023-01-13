3d09077bf2794add9013d33ce366b0a7219b4e09
Mees Postma <mail@meespostma.nl>
2023-01-13 17:17:23 +0100

    Added projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
-import Button from "../components/button";
 import ProjectCard from "../components/projectCard";
 import DefaultLayout from "../layouts/default";
 
-export default function AboutMe() {
+export default function Project() {
   const [projects, setProjects] = useState<Array<any>>([]);
 
   useEffect(() => {
@@ -21,32 +20,20 @@ export default function AboutMe() {
       <main>
         <div className="my-12 mx-auto xl:ml-0 xl:w-7/12 w-11/12">
           <h2 className="text-xl sm:text-4xl font-medium mt-4 md:mt-0">
-            $t('about-me.header.title')
+            $t('projects.header.title')
           </h2>
 
           <p className="leading-6 mt-4 sm:mt-6">
-            $t('about-me.header.description')
+            $t('projects.header.description')
           </p>
-
-          <a href="#">
-            <Button title="Curriculum Vitae" className="mt-6" />
-          </a>
         </div>
 
         <div className="mb-16 mt-8 sm:mt-0 w-11/12 xl:w-full mx-auto">
-          <h2 className="text-xl sm:text-4xl font-medium mt-4 sm:mt-0">
-            $t('about-me.projects.title')
-          </h2>
-
           <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-8 sm:gap-32 mt-8">
             {projects.map((project) => {
               return <ProjectCard key={project.id} project={project} />;
             })}
           </div>
-
-          <a href="/projects">
-            <Button title="$t('about-me.projects.button')" className="mt-6" />
-          </a>
         </div>
       </main>
     </DefaultLayout>
