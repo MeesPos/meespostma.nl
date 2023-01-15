d3d7dc0ddfc32808ad00789cf480cc8a2413f753
Mees Postma <mail@meespostma.nl>
2023-01-15 19:23:31 +0100

    Changed language Switch

--- components/languageSwitch.tsx
+++ components/languageSwitch.tsx
@@ -11,45 +11,20 @@ export default function LanguageSwitch() {
   const { pathname, query, asPath, locale } = router;
 
   return (
-    <div className="text-zinc-800 dark:text-white font-medium">
-      <div
+    <div className="text-zinc-800 dark:text-white cursor-pointer">
+      <Link
         className="flex gap-1 items-center cursor-pointer"
-        onClick={() => setModalOpen(modalOpen ? false : true)}
+        href={{ pathname, query }}
+        as={asPath}
+        locale={locale !== "nl" ? "nl" : "en"}
       >
-        <span>{locale === "nl" ? "Nederlands" : "English"}</span>
-
-        <Icon icon="heroicons-solid:chevron-down" className={modalOpen ? 'rotate-180' : ''} />
-      </div>
-
-      {modalOpen ? (
-        <div className="absolute z-10 mt-3 max-w-5 -translate-x-1/2 transform px-2 sm:px-0">
-          <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-            <div className="relative grid gap-6 bg-white px-5 sm:px-5 sm:py-3 my-2">
-              <Link
-                href={{ pathname, query }}
-                as={asPath}
-                locale={"nl"}
-                className="-m-3 block rounded-md p-3 transition duration-150 ease-in-out hover:bg-gray-50 cursor-pointer"
-              >
-                <p className="text-base font-medium text-gray-900">
-                  Nederlands
-                </p>
-              </Link>
-
-              <Link
-                href={{ pathname, query }}
-                as={asPath}
-                locale={"en"}
-                className="-m-3 block rounded-md p-3 transition duration-150 ease-in-out hover:bg-gray-50 cursor-pointer"
-              >
-                <p className="text-base font-medium text-gray-900">English</p>
-              </Link>
-            </div>
-          </div>
-        </div>
-      ) : (
-        ""
-      )}
+        <p>
+          Switch to{" "}
+          <span className="font-bold">
+            {locale !== "nl" ? "Nederlands" : "English"}
+          </span>
+        </p>
+      </Link>
     </div>
   );
 }
