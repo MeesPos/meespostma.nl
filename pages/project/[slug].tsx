da6462c151facb43e9a6dc04fe0cb644690e1848
Mees Postma <mail@meespostma.nl>
2023-01-15 18:32:02 +0100

    Added project page

--- pages/project/[slug].tsx
+++ pages/project/[slug].tsx
@@ -1,3 +1,19 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import DefaultLayout from "../../layouts/default";
+
 export default function Project() {
-    
+  return (
+    <DefaultLayout>
+      <div></div>
+    </DefaultLayout>
+  );
+}
+
+export async function getServerSideProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["contact", "pages"])),
+      // Will be passed to the page component as props
+    },
+  };
 }
