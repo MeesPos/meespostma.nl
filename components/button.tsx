ea0963447b297b0d0cb60e5200fc4621e956041c
Mees Postma <mail@meespostma.nl>
2023-01-15 18:48:33 +0100

    Fixed className combining classes

--- components/button.tsx
+++ components/button.tsx
@@ -8,10 +8,7 @@ export default function Button({
   return (
     <button
       type="button"
-      className={
-        "inline-flex items-center rounded-md border border-transparent bg-zinc-800 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2 dark:bg-white dark:text-black dark:hover:bg-zinc-100" +
-        className
-      }
+      className={`${className} inline-flex items-center rounded-md border border-transparent bg-zinc-800 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2 dark:bg-white dark:text-black dark:hover:bg-zinc-100`}
     >
       {title}
     </button>
