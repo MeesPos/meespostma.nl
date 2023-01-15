d9db9f2a92e2ddbd94e2d6236f84c3093d3aba66
Mees Postma <mail@meespostma.nl>
2023-01-15 20:43:14 +0100

    Added translations for 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import Button from "../components/button";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 export default function NotFoundError() {
+  const { t } = useTranslation("404");
+
   return (
     <div className="min-h-screen dark:bg-zinc-900 bg-white py-16 px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
       <div className="mx-auto max-w-max">
@@ -12,15 +16,15 @@ export default function NotFoundError() {
           <div className="sm:ml-6">
             <div className="sm:border-l sm:border-gray-200 sm:pl-6">
               <h1 className="text-4xl font-bold tracking-tight dark:text-white text-gray-900 sm:text-5xl">
-                Page not found
+                {t("title")}
               </h1>
               <p className="mt-1 text-base dark:text-gray-300 text-gray-500">
-                Please check the URL in the address bar and try again.
+                {t("description")}
               </p>
             </div>
             <div className="mt-8 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
               <Link href="/">
-                <Button title="Go back home" />
+                <Button title={t("back")} />
               </Link>
             </div>
           </div>
@@ -28,4 +32,12 @@ export default function NotFoundError() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["404"])),
+    },
+  };
 }
