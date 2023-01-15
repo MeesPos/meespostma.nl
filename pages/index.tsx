5675161123ac884507f36c25b66f172eed8883c2
Mees Postma <mail@meespostma.nl>
2023-01-15 16:22:24 +0100

    Added translations to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,13 @@ import Button from "../components/button";
 import DefaultLayout from "../layouts/default";
 import { useEffect, useState } from "react";
 import ProjectCard from "../components/projectCard";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 export default function Home() {
   const [projects, setProjects] = useState<Array<any>>([]);
+
+  const { t } = useTranslation("home");
 
   useEffect(() => {
     async function getDatabase() {
@@ -22,20 +26,17 @@ export default function Home() {
       <main className="w-11/12 md:w-auto mx-auto">
         <div className="flex flex-col-reverse md:flex-row md:gap-12 lg:gap-48 items-center sm:my-12">
           <div className="flex-initial">
-            <h2
-              className="text-xl sm:text-4xl font-medium mt-4 md:mt-0"
-              v-text="$t('home.header.title')"
+            <h2 className="text-xl sm:text-4xl font-medium mt-4 md:mt-0">
+              {t("header.title")}
+            </h2>
+
+            <p
+              className="leading-6 mt-4 sm:mt-6"
+              dangerouslySetInnerHTML={{ __html: t("header.description") }}
             />
 
-            <p className="leading-6 mt-4 sm:mt-6">
-              $t('home.header.description')
-            </p>
-
             <a href="/about-me">
-              <Button
-                title="$t('home.header.button')"
-                className="mt-4 sm:mt-6"
-              />
+              <Button title={t("header.button")} className="mt-4 sm:mt-6" />
             </a>
           </div>
 
@@ -50,10 +51,9 @@ export default function Home() {
         </div>
 
         <div className="mb-16 mt-8 sm:mt-0">
-          <h2
-            className="text-xl sm:text-4xl font-medium mt-4 sm:mt-0"
-            v-text="$t('home.projects.title')"
-          />
+          <h2 className="text-xl sm:text-4xl font-medium mt-4 sm:mt-0">
+            {t("projects.title")}
+          </h2>
 
           <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-8 sm:gap-32 mt-8">
             {projects.map((project) => {
@@ -62,10 +62,19 @@ export default function Home() {
           </div>
 
           <a href="/projects">
-            <Button title="$t('home.projects.button')" className="mt-6" />
+            <Button title={t("projects.button")} className="mt-6" />
           </a>
         </div>
       </main>
     </DefaultLayout>
   );
+}
+
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["home"])),
+      // Will be passed to the page component as props
+    },
+  };
 }
