a8990252eb123d47c8fb0f13ddcef77188d33356
Mees Postma <mail@meespostma.nl>
2023-06-19 16:44:41 +0200

    Fixed error with Vercel (I think)

--- prisma/migrations/20230530155527_create_project_table/migration.sql
+++ prisma/migrations/20230530155527_create_project_table/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE `Project` (
+CREATE TABLE `Projects` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
