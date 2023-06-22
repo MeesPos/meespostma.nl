66c25cf86ca1d1198a41404972d1cd5cfe0cc001
Mees Postma <mail@meespostma.nl>
2023-06-22 11:47:51 +0200

    Added authentication for dashboard

--- layouts/dashboard.tsx
+++ layouts/dashboard.tsx
@@ -1,11 +1,22 @@
+import { useEffect } from "react";
 import Footer from "../components/footer";
 import Navigation from "../components/navigation";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { status } = useSession();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") router.push("/login");
+  });
+
   return (
     <div className="min-h-screen bg-zinc-50 dark:bg-black dark:text-white">
       <div className="max-w-screen-2xl min-h-screen mx-auto bg-white dark:bg-zinc-900">
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,17 @@ import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
-      <Component {...pageProps} />
+    <SessionProvider>
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Component {...pageProps} />
 
-      <Toaster position="top-right" />
-    </ThemeProvider>
+        <Toaster position="top-right" />
+      </ThemeProvider>
+    </SessionProvider>
   );
 };
 
